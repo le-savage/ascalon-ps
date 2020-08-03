3611fe75b722b26aa730442551c982f0f060dddf
jack <Av2Z%cVbA7rx>
2020-08-03 01:41:55 +0100

    Back to Null's Scythe version

--- src/com/janus/world/content/combat/weapon/effects/impl/weapon/ScytheOfVitur.java
+++ src/com/janus/world/content/combat/weapon/effects/impl/weapon/ScytheOfVitur.java
@@ -27,7 +27,7 @@ public class ScytheOfVitur extends ItemEffect {
 
     @Override
     public int hitAmount(Character attacker, Character victim) {
-        int hitAmount = 1;
+        /*int hitAmount = 1;
         int victims = targets(attacker, victim).size() + 1; //add 1 for the victim themself
         if(victim.getSize() >= 2) {
             hitAmount = 3;
@@ -38,7 +38,8 @@ public class ScytheOfVitur extends ItemEffect {
                 hitAmount = 3;
         }
         System.out.println("Hit Amount: " + hitAmount + " - victims size " + victims);
-        return hitAmount;
+        return hitAmount;*/
+        return Misc.random(1, 3);
     }
 
 
