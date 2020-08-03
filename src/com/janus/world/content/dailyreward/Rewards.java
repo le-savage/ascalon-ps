5ef86caa6ae98186be4002f8a10763ff5c4e9ad8
jack <Av2Z%cVbA7rx>
2020-08-03 11:42:07 +0100

    Daily Rewards Updated

--- src/com/janus/world/content/dailyreward/Rewards.java
+++ src/com/janus/world/content/dailyreward/Rewards.java
@@ -6,36 +6,36 @@ public class Rewards {
 
     protected static Item[] loot = {
             new Item(6199, 3),//1
-            new Item(2581, 1),//2
-            new Item(537, 200),//3
-            new Item(21061, 5),//4
-            new Item(18937, 5),//5
-            new Item(2577, 1),//6
-            new Item(18782, 1),//7
-            new Item(6585, 1),//8
-            new Item(11732, 1),//9
-            new Item(21062, 5),//10
-            new Item(6733, 1),//11
-            new Item(6737, 1),//14
-            new Item(11938, 1),//15
-            new Item(4151, 1),//16
-            new Item(995, 100000000),//17
-            new Item(20059, 1),//18
-            new Item(995, 200000000),//19
-            new Item(11718, 1),//20
-            new Item(11722, 1),//21
-            new Item(11720, 1),//22
-            new Item(11724, 1),//23
-            new Item(11726, 1),//23
-            new Item(21026, 1),//24
-            new Item(6829, 1),//25
-            new Item(6183, 1),//26
-            new Item(15355, 1),//27
-            new Item(14484, 1),//28
-            new Item(11694, 1),//29
-            new Item(12926, 1),//30
-            new Item(12284, 1),//31
-            new Item(14664, 1)//32
+            new Item(21026, 1),//2
+            new Item(13887, 1),//3
+            new Item(13893, 1),//4
+            new Item(6831, 1),//5
+            new Item(995, 200000000),//6
+            new Item(19335, 1),//7
+            new Item(1543, 5),//8
+            new Item(18831, 200),//9
+            new Item(4716, 1),//10
+            new Item(4718, 1),//11
+            new Item(4720, 1),//14
+            new Item(4722, 1),//15
+            new Item(15359, 1),//16
+            new Item(995, 500000000),//17
+            new Item(20057, 1),//18
+            new Item(537, 150),//19
+            new Item(13051, 1),//20
+            new Item(10362, 1),//21
+            new Item(18902, 1),//22
+            new Item(12708, 1),//23
+            new Item(4453, 1),//23
+            new Item(18782, 1),//24
+            new Item(21066, 5),//25
+            new Item(15356, 1),//26
+            new Item(13744, 1),//27
+            new Item(535, 500),//28
+            new Item(13239, 1),//29
+            new Item(18831, 300),//30
+            new Item(6832, 1),//31
+            new Item(962, 1)//32
 
     };
 }
