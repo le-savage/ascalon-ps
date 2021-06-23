8c9adf5f767df58d3434ed2ac008d989b875de03
martijn <djsilver19@outlook.com>
2021-06-24 01:14:19 +0200

    final tweaks for now

--- src/com/janus/world/content/combat/tieredbosses/StatSetups.java
+++ src/com/janus/world/content/combat/tieredbosses/StatSetups.java
@@ -11,25 +11,25 @@ public enum StatSetups {
         DEFAULT(1, 1, 1, 1, 1, 10, 10),
 
         //First
-        FROST_DRAGON(80, 50, 60, 1, 15, 990, 70),
-        BLACK_DRAGON(90, 60,    20, 50, 99, 990, 70),
-        KBD_WAVE_ONE(70, 70, 70, 1, 1, 990, 770),
+        FROST_DRAGON(80, 80, 80, 1, 15, 990, 700),
+        BLACK_DRAGON(90, 80,    80, 50, 99, 990, 700),
+        KBD_WAVE_ONE(80, 80, 80, 1, 1, 990, 770),
         //Second Wave
-        TORMENTED_DEMON(80, 80, 80, 80, 80, 880, 70),
-        CHAOS_ELEMENTAL(80, 80, 80, 80, 99, 990, 70),
+        TORMENTED_DEMON(80, 80, 80, 80, 80, 880, 700),
+        CHAOS_ELEMENTAL(80, 80, 80, 80, 99, 990, 700),
         DAGANNOTH_PRIME(80, 80, 80, 80, 80, 990, 990),
         //Third
-        DAGANNOTH_SUPREME(80, 80, 80, 80, 80, 880, 70),
-        BARREL_CHEST(70, 70, 70, 1, 70, 990, 70),
-        CERBERUS(60, 60, 60, 60, 99, 990, 70),
+        DAGANNOTH_SUPREME(80, 80, 80, 80, 80, 880, 700),
+        BARREL_CHEST(80, 80, 80, 80, 80, 990, 700),
+        CERBERUS(80, 80, 80, 60, 99, 990, 700),
         //Fourth
-        THERMONUCLEAR_SMOKEDEVIL(70, 70, 70, 1, 80, 880, 70),
-        VENENATIS(70, 70, 70, 99, 99, 80, 80),
-        DAGANNOTH_REX(70, 70, 60, 1, 70, 80, 70),
+        THERMONUCLEAR_SMOKEDEVIL(80, 80, 80, 80, 80, 880, 700),
+        VENENATIS(80, 80, 80, 80, 80, 80, 800),
+        DAGANNOTH_REX(80, 80, 80, 80, 70, 80, 700),
         //Fifth
-        CRAZY_LVL2_MAN(80, 80, 80, 80, 80, 880, 70),
-        SCORPIA(80, 80, 80, 99, 99, 990, 70),
-        BORK(80, 80, 80, 99, 99, 990, 70);
+        CRAZY_LVL2_MAN(80, 80, 80, 80, 80, 880, 700),
+        SCORPIA(80, 80, 80, 80, 80, 990, 700),
+        BORK(80, 80, 80, 80, 80, 990, 700);
 
 
         private final int attack;
